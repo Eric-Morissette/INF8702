f17e42ea0fa62c77d6d7f2deb29e7ddde5f081ee
unknown <ermor2@L4818-01.gigl.polymtl.ca>
2015-11-19 12:52:56 -0500

    Commentaires/leger changement du FragShader

--- Projet/nuanceurs/nuanceurFragments.glsl
+++ Projet/nuanceurs/nuanceurFragments.glsl
@@ -8,13 +8,18 @@
 
 uniform int renderStyle;
 
+// Cel Shading
+float seuil = 10.0;
+// Cel Shading
+
+// Gooch Shading
 vec3 goochWarmColor = vec3(1.0, 1.0, 0.0);
 vec3 goochColdColor = vec3(0.0, 0.0, 1.0);
+// Gooch Shading
 
 void main(void)
 {
 	vec4 color;
-	float seuil = 5.0;
 
 	color = gl_Color;
 
@@ -24,16 +29,18 @@ void main(void)
 		// Do nothing
 		break;
 	case 2: // Cel Shading
+        // prototype meilleur
+        // se baser sur l'intensite, exemple: http://rbwhitaker.wikidot.com/toon-shader
 		color.r = (floor(color.r * seuil)) / seuil;
 		color.g = (floor(color.g * seuil)) / seuil;
 		color.b = (floor(color.b * seuil)) / seuil;
 		break;
 	case 3: // 3 - Gooch Shading
-		color.r = (color.r * goochWarmColor.r) + ((1 - color.r) * goochColdColor.r);
-		//prototype meilleur
-		//color.r = color.r * ((intensite * goochWarmColor.r) + ((1 - intensite) * goochColdColor.r));
-		color.g = (color.g * goochWarmColor.g) + ((1 - color.g) * goochColdColor.g);
-		color.b = (color.b * goochWarmColor.b) + ((1 - color.b) * goochColdColor.b);
+		color.r = (color.r * goochWarmColor.r) + ((1.0 - color.r) * goochColdColor.r);
+		// prototype meilleur
+		// color.r = color.r * ((intensite * goochWarmColor.r) + ((1 - intensite) * goochColdColor.r));
+		color.g = (color.g * goochWarmColor.g) + ((1.0 - color.g) * goochColdColor.g);
+		color.b = (color.b * goochWarmColor.b) + ((1.0 - color.b) * goochColdColor.b);
 		break;
 	case 0: // 0 - No Render
 	default:
